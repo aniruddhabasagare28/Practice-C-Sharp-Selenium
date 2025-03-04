3cc5f235eb2e216302d32802ae992661be1d5dac
Aniruddha <aniruddhabasagare28@gmail.com>
2025-03-04 10:59:57 +0530

    adding more test cases

--- SeleniumFramework/Test/LoginTests.cs
+++ SeleniumFramework/Test/LoginTests.cs
@@ -24,10 +24,88 @@ namespace SeleniumFramework.Test
 
             Assert.That(Driver.Url, Is.EqualTo("https://the-internet.herokuapp.com/secure"), "Login failed!");
         }
+        [Test, Category("Regression")]
+        public void Test_LoginFunctionality3()
+        {
+            Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");  // Dummy login site
+
+            LoginPage loginPage = new LoginPage(Driver);
+            loginPage.EnterUsername("tomsmith");
+            loginPage.EnterPassword("SuperSecretPassword!");
+            loginPage.ClickLogin();
+
+            Assert.That(Driver.Url, Is.EqualTo("https://the-internet.herokuapp.com/secure"), "Login failed!");
+        }
+        [Test, Category("Regression")]
+        public void Test_LoginFunctionality1()
+        {
+            Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");  // Dummy login site
+
+            LoginPage loginPage = new LoginPage(Driver);
+            loginPage.EnterUsername("tomsmith");
+            loginPage.EnterPassword("SuperSecretPassword!");
+            loginPage.ClickLogin();
+
+            Assert.That(Driver.Url, Is.EqualTo("https://the-internet.herokuapp.com/secure"), "Login failed!");
+        }
+        [Test, Category("Regression")]
+        public void Test_LoginFunctionality2()
+        {
+            Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");  // Dummy login site
+
+            LoginPage loginPage = new LoginPage(Driver);
+            loginPage.EnterUsername("tomsmith");
+            loginPage.EnterPassword("SuperSecretPassword!");
+            loginPage.ClickLogin();
+
+            Assert.That(Driver.Url, Is.EqualTo("https://the-internet.herokuapp.com/secure"), "Login failed!");
+        }
 
         [Test, Category("Smoke")]
         
         public void Test_LogoutFunctionality()
+        {
+            Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");  // Dummy login site
+
+            LoginPage loginPage = new LoginPage(Driver);
+            loginPage.EnterUsername("tomsmith");
+            loginPage.EnterPassword("SuperSecretPassword!");
+            loginPage.ClickLogin();
+
+            Assert.That(Driver.Url, Is.EqualTo("https://the-internet.herokuapp.com/secure"), "Login failed!");
+        }
+
+        [Test, Category("Smoke")]
+
+        public void Test_LogoutFunctionality1()
+        {
+            Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");  // Dummy login site
+
+            LoginPage loginPage = new LoginPage(Driver);
+            loginPage.EnterUsername("tomsmith");
+            loginPage.EnterPassword("SuperSecretPassword!");
+            loginPage.ClickLogin();
+
+            Assert.That(Driver.Url, Is.EqualTo("https://the-internet.herokuapp.com/secure"), "Login failed!");
+        }
+
+        [Test, Category("Smoke")]
+
+        public void Test_LogoutFunctionality2()
+        {
+            Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");  // Dummy login site
+
+            LoginPage loginPage = new LoginPage(Driver);
+            loginPage.EnterUsername("tomsmith");
+            loginPage.EnterPassword("SuperSecretPassword!");
+            loginPage.ClickLogin();
+
+            Assert.That(Driver.Url, Is.EqualTo("https://the-internet.herokuapp.com/secure"), "Login failed!");
+        }
+
+        [Test, Category("Smoke")]
+
+        public void Test_LogoutFunctionality3()
         {
             Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");  // Dummy login site
 
